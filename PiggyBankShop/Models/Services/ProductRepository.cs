c66a3edd96a4d30207cc8bd186950c6a6a25a58e
johannpa <johann_paque@hotmail.fr>
2023-09-25 19:06:59 +0200

    Modify the repository class

--- PiggyBankShop/Models/Services/ProductRepository.cs
+++ PiggyBankShop/Models/Services/ProductRepository.cs
@@ -1,28 +1,30 @@
-﻿using PiggyBankShop.Models.Interfaces;
+﻿using PiggyBankShop.Data;
+using PiggyBankShop.Models.Interfaces;
 
 namespace PiggyBankShop.Models.Services
 {
     public class ProductRepository : IProductRepository
     {
-        private List<Product> ProductsList = new List<Product>()
+        private PiggyBankShopDbContext dbContext;
+
+        public ProductRepository(PiggyBankShopDbContext dbContext)
         {
-            new Product { Id = 1, Name = "Big Pig", Price = 35, Detail = "Les tirelires sont populaires auprès des enfants pour les aider à apprendre à économiser de l'argent...", ImageUrl="https://t3.ftcdn.net/jpg/00/61/45/26/360_F_61452654_zeYz36bPOmoRvlbya4ZfUebgQBHmnaXd.jpg"},
-            new Product { Id = 2, Name = "Middle Pig", Price = 25, Detail = "Les tirelires sont populaires auprès des enfants pour les aider à apprendre à économiser de l'argent ...", ImageUrl="https://t3.ftcdn.net/jpg/00/61/45/26/360_F_61452654_zeYz36bPOmoRvlbya4ZfUebgQBHmnaXd.jpg"},
-            new Product { Id = 3, Name = "Little Pig", Price = 15, Detail = "Les tirelires sont populaires auprès des enfants pour les aider à apprendre à économiser de l'argent ...", ImageUrl="https://t3.ftcdn.net/jpg/00/61/45/26/360_F_61452654_zeYz36bPOmoRvlbya4ZfUebgQBHmnaXd.jpg"},
-        };
-        public IEnumerable<Product> GetAllProducts()
-        {
-            return ProductsList;
+            this.dbContext = dbContext;
         }
 
-        public Product GetProductDetail(int id)
+        public IEnumerable<Product> GetAllProducts()
         {
-            return ProductsList.FirstOrDefault(p => p.Id == id);
+            return dbContext.Products;
+        }
+
+        public Product? GetProductDetail(int id)
+        {
+            return dbContext.Products.FirstOrDefault(p => p.Id == id);
         }
 
         public IEnumerable<Product> GetTrendingProducts()
         {
-            return ProductsList.Where(p => p.IsTrendingProduct);
+            return dbContext.Products.Where(p => p.IsTrendingProduct);
         }
     }
 }
