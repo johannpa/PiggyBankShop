68c3af68d14db83f71993796b73c2a367e83db8c
johannpa <johann_paque@hotmail.fr>
2023-09-25 14:24:40 +0200

    Create Controller

---
+++ PiggyBankShop/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PiggyBankShop.Models.Interfaces;
+
+namespace PiggyBankShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Shop()
+        {
+            return View(productRepository.GetAllProducts());
+        }
+    }
+}
