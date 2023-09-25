3e28f3a6988a7268ec2fad182f0c37e3322e12e1
johannpa <johann_paque@hotmail.fr>
2023-09-25 15:20:14 +0200

    Add packages nugets Microsoft.EntityFrameworkCore and Microsoft.EntityFrameworkCore

---
+++ PiggyBankShop/Data/PiggyBankShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PiggyBankShop.Models;
+
+namespace PiggyBankShop.Data
+{
+    public class PiggyBankShopDbContext : DbContext
+    {
+        public PiggyBankShopDbContext(DbContextOptions<PiggyBankShopDbContext> options):base(options)
+        {
+        }
+        public DbSet<Product> Products{ get; set; }
+    }
+}
