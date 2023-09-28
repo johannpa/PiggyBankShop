942e24a82c2b26ce6ea13e0577e09b479f75c192
johannpa <johann_paque@hotmail.fr>
2023-09-28 17:06:58 +0200

    Populate trending products data

--- PiggyBankShop/Controllers/HomeController.cs
+++ PiggyBankShop/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PiggyBankShop.Models.Interfaces;
 
 namespace PiggyBankShop.Controllers
 {
     public class HomeController : Controller
     {
+        private IProductRepository _productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            
+            return View(_productRepository.GetTrendingProducts());
         }
     }
 }
