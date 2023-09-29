726128461289ab94253e01f00f2c7c1b0a13225c
johannpa <johann_paque@hotmail.fr>
2023-09-29 21:41:40 +0200

    Working with Session management and shopping cart methods

--- PiggyBankShop/Models/Services/ShoppingCartRepository.cs
+++ PiggyBankShop/Models/Services/ShoppingCartRepository.cs
@@ -1,4 +1,5 @@
-﻿using PiggyBankShop.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using PiggyBankShop.Data;
 using PiggyBankShop.Models.Interfaces;
 
 namespace PiggyBankShop.Models.Services
@@ -11,30 +12,80 @@ namespace PiggyBankShop.Models.Services
             this.dbContext = dbContext;
         }
         public List<ShoppingCartItem>? ShoppingCartItems { get ; set ; }
+        public string? ShoppingCartId { get; set; }
+
+        public static ShoppingCartRepository GetCart(IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
+
+            PiggyBankShopDbContext context = services.GetService<PiggyBankShopDbContext>() ?? throw new Exception("Error initializing PiggyBankShopDbContext");
+
+            string cartId = session?.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session?.SetString("CartId", cartId);
+
+            return new ShoppingCartRepository(context) { ShoppingCartId = cartId };
+        }
 
         public void AddToCart(Product product)
         {
-            throw new NotImplementedException();
+            var shoppingCartItem = dbContext.ShoppingCartItems.FirstOrDefault(s => s.Product.Id == product.Id && s.ShoppingCartId == ShoppingCartId);
+            if (shoppingCartItem != null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Product = product,
+                    Qty = 1
+                };
+                dbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Qty++;
+            }
+            dbContext.SaveChanges();
         }
 
         public void CleanCart()
         {
-            throw new NotImplementedException();
+            var cartItems = dbContext.ShoppingCartItems.Where(s => s.ShoppingCartId == ShoppingCartId);
+            dbContext.ShoppingCartItems.RemoveRange(cartItems);
+            dbContext.SaveChanges();
         }
 
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
-            throw new NotImplementedException();
+            return ShoppingCartItems ??= dbContext.ShoppingCartItems.Where(s => s.ShoppingCartId == ShoppingCartId)
+                .Include(p => p.Product).ToList();
         }
 
         public decimal GetShoppingCartTotal()
         {
-            throw new NotImplementedException();
+            var totalCost = dbContext.ShoppingCartItems.Where(s => s.ShoppingCartId == ShoppingCartId)
+                .Select(s => s.Product.Price * s.Qty).Sum();
+            return totalCost;
         }
 
         public int RemoveFromCart(Product product)
         {
-            throw new NotImplementedException();
+            var shoppingCartItem = dbContext.ShoppingCartItems.FirstOrDefault(s => s.Product.Id == product.Id && s.ShoppingCartId == ShoppingCartId);
+            var quantity = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if(shoppingCartItem.Qty > 1)
+                {
+                    shoppingCartItem.Qty--;
+                    quantity = shoppingCartItem.Qty;
+                }
+                else
+                {
+                    dbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+            dbContext.SaveChanges();
+            return quantity;
         }
     }
 }
