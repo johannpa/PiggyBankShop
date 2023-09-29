158cbc6334ab97c23d489118b3b3c61fd968533d
johannpa <johann_paque@hotmail.fr>
2023-09-29 22:05:23 +0200

    Shopping cart controller

---
+++ PiggyBankShop/Controllers/ShoppingCartController1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PiggyBankShop.Models.Interfaces;
+
+namespace PiggyBankShop.Controllers
+{
+    public class ShoppingCartController1 : Controller
+    {
+        private IShoppingCartRepository shoppingCartRepository;
+        private IProductRepository productRepository;
+
+        public ShoppingCartController1(IShoppingCartRepository shoppingCartRepository, IProductRepository productRepository)
+        {
+            this.shoppingCartRepository = shoppingCartRepository;
+            this.productRepository = productRepository;
+        }
+        public IActionResult Index()
+        {
+            var items = shoppingCartRepository.GetShoppingCartItems();
+            shoppingCartRepository.ShoppingCartItems = items;
+            return View(items);
+        }
+
+        public RedirectToActionResult AddToShoppingCart(int pId)
+        {
+            var product = productRepository.GetAllProducts().FirstOrDefault(p => p.Id == pId);
+            if(product != null)
+            {
+                shoppingCartRepository.AddToCart(product);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoveFromShoppingCart(int pId)
+        {
+            var product = productRepository.GetAllProducts().FirstOrDefault(p => p.Id == pId);
+            if (product != null)
+            {
+                shoppingCartRepository.RemoveFromCart(product);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
