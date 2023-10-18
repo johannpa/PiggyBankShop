4a60976199b51685781f5f47c01271464bdf194a
johannpa <johann_paque@hotmail.fr>
2023-10-18 14:35:55 +0200

    Create Orders Controller

---
+++ PiggyBankShop/Controllers/OrdersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PiggyBankShop.Models;
+using PiggyBankShop.Models.Interfaces;
+
+namespace PiggyBankShop.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private IOrderRepository _orderRepository;
+        private IShoppingCartRepository _shoppingCartRepository;
+        public OrdersController(
+            IOrderRepository orderRepository, 
+            IShoppingCartRepository shoppingCartRepository)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            _orderRepository.PlaceOrder(order);
+            _shoppingCartRepository.CleanCart();
+            return Redirect("CheckoutComplete");
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            return View();
+        }
+    }
+}
